46a222a5d59c56cfc51ff4e3ca297a362e17eaed
Albertosantana96 <albertopeinado96@gmail.com>
2018-12-10 21:55:06 +0000

    v3.2

--- gestionLugares.php
+++ gestionLugares.php
@@ -19,32 +19,36 @@
   $resultado = mysqli_query ($con, $sql);
   $num_filas = mysqli_affected_rows($con);
 ?>
+<div class ="cuadro-tabla">
+  <a href=añadirRuta.php> <button class = "boton-añadir" type="button"> Añadir Ruta Nueva </button> </a>
+  <p></p>
 
-<table class = 'tabla-ruta'>
-  <thead>
-  <tr>
-    <th>Nombre del lugar</th>
-    <th>ID</th>
-    <th></th>
-    <th></th>
-  </tr>
-  </thead>";
-  
-  <?php
-    if ($num_filas > 0) {
-      while($fila = mysqli_fetch_assoc($resultado)) {
-        echo "<tbody>";
-        echo "<tr>";
-        echo "<td>" . $fila['nombre'] . "</td>";
-        echo "<td>" . $fila['id'] . "</td>";
-        echo "<td><a href=\"modificarLugares.php?id=" . $fila['id'] . "\"> <button type=\"button\"> Modificar </button> </a></td>";
-        echo "<td><a href=\"eliminarLugares.php?id=" . $fila['id'] . "\"> <button type=\"button\"> Eliminar </button> </a></td>";
-        echo "</tr>";
-        echo "</tbody>";
-      }
-    }
+  <table class = 'tabla-ruta'>
+    <thead>
+    <tr>
+      <th>id</th>
+      <th>Nombre de la Ruta</th>
+      <th></th>
+      <th></th>
+    </tr>
+    </thead>
     
-    echo "</table>";
-  ?>
+    <?php
+      if ($num_filas > 0) {
+        while($fila = mysqli_fetch_assoc($resultado)) {
+          echo "<tbody>";
+          echo "<tr>";
+          echo "<td>" . $fila['id'] . "</td>";
+          echo "<td>" . $fila['nombre'] . "</td>";
+          echo "<td><a href=\"modificarRuta.php?id=" . $fila['id'] . "\"> <button class =\"boton-modificar\" type=\"button\"> Modificar </button> </a></td>";
+          echo "<td><a href=\"eliminarRuta.php?id=" . $fila['id'] . "\"> <button class =\"boton-eliminar\" type=\"button\"> Eliminar </button> </a></td>";
+          echo "</tr>";
+          echo "</tbody>";
+        }
+      }
+      
+      echo "</table>";
+    ?>
+</div>
 </body>
 </html>
--- modificarLugares.php
+++ modificarLugares.php
@@ -14,12 +14,61 @@
 ?>
 
 <?php
-  $con=mysqli_connect("localhost","root","","sombrilla");
-  $sql = "SELECT * FROM ruta ORDER BY id ASC";
+  $id = $_GET['id'];
+  $con = mysqli_connect("localhost","root","","sombrilla");
+  $sql = "SELECT nombre, descripcion FROM  Ruta, DescripcionRuta WHERE Ruta.id = $id";
   $resultado = mysqli_query ($con, $sql);
-  $num_filas = mysqli_affected_rows($con);
+  $fila = mysqli_fetch_assoc($resultado);
 ?>
 
+<form class ="mod-form" action='' method='post' accept-charset='UTF-8'>
+  <div class = 'form-mod'>
+
+    <h2> Formulario de modificacion de rutas </h2>
+    <label for='id'></label>
+    <input type="hidden" id = "id" name="id" value="<?php echo $id; ?>"/>
+
+    <div class='form-row-nombre'>
+      <label for='nombre'>Nombre</label>
+      <input  name="nombre" id="nombre" type="text" value="<?php echo $fila['nombre']?>"/>
+    </div>
+
+    <div class='form-row'>
+      <label for='descripcion'>Descripcion</label>
+      <input type="text" id="descripcion" name="descripcion" value="<?php echo $fila['descripcion']?>"/>
+    </div>
+
+    <input type="submit" class="boton-modificar" name="submit" value="Modificar">
+
+  </div>
+</form>
+
+<?php
+  if (isset($_POST['submit'])) {
+    // confirm that the 'id' value is a valid integer before getting the form data
+    if (is_numeric($_POST['id'])) {
+      // get form data, making sure it is valid
+      $id = $_POST['id'];
+      $ruta = htmlspecialchars($_POST['nombre']);
+      $descripcion = htmlspecialchars($_POST['descripcion']);
+      
+      // check that firstname/lastname fields are both filled in
+      if ($ruta == '' || $descripcion == '') {
+        // generate error message
+        $error = 'ERROR: Please fill in all required fields!';
+      } else {
+        mysqli_query ($con, "UPDATE Ruta SET nombre='$ruta' WHERE id='$id'")
+        or die(mysqli_error($con));
+        mysqli_query ($con, "UPDATE DescripcionRuta SET descripcion='$descripcion' WHERE ruta='$id'")
+        or die(mysqli_error($con));
+        header("Location: modificarRuta.php?id=$id");
+      }
+    } else {
+      // if the 'id' isn't valid, display an error
+      echo 'Error!';
+    }
+  }
+?>
 
 </body>
 </html>
